15f3d645221f1ffc5f4a5d2a29879b1e846811e5
Jatin Prakash <jatinprakash1511@gmail.com>
2025-12-29 18:52:49 -0500

    tp for attention

--- mlsys/tp.py
+++ mlsys/tp.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
 
+import math
+
 import torch
 import torch.nn as nn
 import torch.distributed as dist
+
+import einops
 
 @dataclass
 class ConfigTP:
     num_processes: int = 2
 
     dim: int = 256
+    head_dim: int = 64
 
     def __init__(self):
         assert self.dim % self.num_processes == 0
@@ -80,3 +85,45 @@ class TPFeedForward(nn.Module):
 
         return x
 
+
+class Attention(nn.Module):
+
+    def __init__(self, config: ConfigTP):
+
+        self.config = config
+        self.dim = config.dim
+        self.dim_reduced = config.dim_reduced
+        self.head_dim = config.head_dim
+
+        # [D, D_red]
+        self.wq = nn.Linear(self.dim, self.dim_reduced, bias=False)
+        self.wk = nn.Linear(self.dim, self.dim_reduced, bias=False)
+        self.wv = nn.Linear(self.dim, self.dim_reduced, bias=False)
+
+        # [D_red, D]
+        self.wo = nn.Linear(self.dim_reduced, self.dim, bias=False)
+
+    def forward(self, x):
+
+        x = f(x)
+
+        q = self.wq(x) # [B, N, D]
+        k = self.wk(x)
+        v = self.wv(x)
+
+        local_heads = self.dim_reduced // self.head_dim
+
+        q = einops.rearrange(q, "b n (h d) -> b h n d", h=local_heads, d=self.head_dim)
+        k = einops.rearrange(k, "b n (h d) -> b h n d", h=local_heads, d=self.head_dim)
+        v = einops.rearrange(v, "b n (h d) -> b h n d", h=local_heads, d=self.head_dim)
+
+        # do attention
+        y = torch.nn.functional.scaled_dot_product_attention(q, k, v, scale=(1/math.sqrt(self.head_dim)))
+
+        y = einops.rearrange(y, "b h n d -> b n (h d)", h=local_heads, d=self.head_dim) # [B, N, D_red]
+
+        o = self.wo(y)
+
+        o = g(o)
+
+        return o
