dbfa9b5e7bc43ce4fd9b2722668cd942371c9210
Jatin Prakash <jatinprakash1511@gmail.com>
2026-05-12 11:37:27 -0400

    (grouped) latent attention

---
+++ decoding/latent.py
@@ -0,0 +1,82 @@
+import torch
+import einops
+
+B = 4
+L = 128
+
+D = 1024
+H = 16
+d = 64
+c = 256 # usually chosen as 4 * d
+
+# now there are these weights
+W_q
+W_c_to_k # instead of W_k
+W_c_to_v # instead of W_k
+W_o
+
+assert H*d == D
+
+# Assume single GPU setup only!
+
+# query vector in HBM
+q = torch.randn((B, 1, D)) # [B, 1, D]
+q = einops.reshape(q, "b 1 (h d) -> b h 1 d", h=H, d=d)
+
+# NOTE
+q_c = q @ w_c_to_k.T # # [B, 1, c]
+
+# NOTE
+# latent cache in HBM
+latent_cache = torch.randn((B, 1, c))
+
+### kernel starts below
+### everything below happens in SRAM
+
+# move q to SRAM
+# create buffers for denominator and output vector
+# NOTE
+y = torch.zeros((B, 1, c)) # stays in SRAM
+den_sum = torch.zeros((B, H, 1, 1))
+den_max = torch.ones((B, H, 1, 1)) * (-1e9)
+
+block_size = 16
+
+for i in range(0, L, block_size):
+
+    start = i
+    end = i + block_size
+
+    # load cache from HBM to SRAM
+    # NOTE: we load less stuff from HBM
+    cache = latent_cache[:, :, start:end, :]
+
+    # now here, we could map to keys-values
+    # and then duplicate keys-values to match heads of query
+
+    # (q @ (c @ w_c_to_k).T) = (q @ w_c_to_k.T) @ c.T 
+    # i.e. "do attention in latent space"
+
+    # do attention
+    # NOTE: the dot product is happening with 256 sized vectors, rather than 64
+    # there's some trade-off with block_size
+    s = q @ cache.T # [B, H, 1, block_size]
+
+    cur_max = torch.max(s, dim=-1, keepdim=True) # [B, H, 1, 1]
+    cur_max = torch.maximum(cur_max, den_max) # [B, H, 1, 1]
+
+    # correct den_sum
+    den_sum = den_sum * torch.exp(den_max - cur_max) + torch.exp(s - cur_max).sum(dim=-1, keepdim=True) # [B, H, 1, 1]
+
+    # [B, H, 1, block_size] @ [B, H, block_size, d] = [B, H, 1, d]
+    y = y * torch.exp(den_max - cur_max) + ((torch.exp(s - cur_max) / den_sum) @ value) 
+
+    den_max = cur_max
+
+# NOTE
+return y # [B, 1, c]
+
+
+# NOTE: now later, do
+# deepseek calls it "fused weights"
+o = y @ (w_to_v @ w_o) # [B, 1, D]
